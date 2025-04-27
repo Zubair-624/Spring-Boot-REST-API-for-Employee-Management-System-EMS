2a4ff61af2488ff03b153a960b24117f529938cb
Zubair624 <zubairmazumder624@gmail.com>
2025-04-28 01:39:37 +0600

    initial commit

---
+++ src/main/java/com/emsbackend/springbootrestapiforemployeemanagementsystemems/SpringBootRestApiForEmployeeManagementSystemEmsApplication.java
@@ -0,0 +1,13 @@
+package com.emsbackend.springbootrestapiforemployeemanagementsystemems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiForEmployeeManagementSystemEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiForEmployeeManagementSystemEmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emsbackend/springbootrestapiforemployeemanagementsystemems/SpringBootRestApiForEmployeeManagementSystemEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsbackend.springbootrestapiforemployeemanagementsystemems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiForEmployeeManagementSystemEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
